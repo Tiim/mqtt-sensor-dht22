cf98e45f28ec2f7032b9a44efdef7c412f267947
Tiim <tim.bachmann96@gmail.com>
2019-08-14 22:12:03 +0200

    Add first working prototype

---
+++ src/keys.h
@@ -0,0 +1,13 @@
+
+#if __has_include("keys_private.h")
+
+#include "keys_private.h"
+
+#else
+
+#define STASSID "WIFI NAME"
+#define STAPSK "WIFI PASSWORD"
+#define MQTT_BROKER "mqtt.broker.host"
+#define MQTT_USER "MQTTUsername"
+#define MQTT_PASS "MQTTPassword"
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,105 @@
+/*
+    This sketch establishes a TCP connection to a "quote of the day" service.
+    It sends a "hello" message, and then prints received data.
+*/
 
-#include "DHT.h"
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <DHT.h>
+#include <ArduinoJson.h>
+
+#include "keys.h"
+
+const char *ssid = STASSID;
+const char *password = STAPSK;
+
+const char *mqtt_user = MQTT_USER;
+const char *mqtt_password = MQTT_PASS;
 
 #define DHTTYPE DHT22 // DHT 22  (AM2302), AM2321
 
 #define DHTPIN D2
 
 DHT dht(DHTPIN, DHTTYPE);
+WiFiClient client;
+PubSubClient mqtt(client);
+
+void publish();
+void wifi_connect();
+void mqtt_connect();
 
 void setup()
 {
-  Serial.begin(9600);
-  Serial.println("DHT22 Test!");
-
+  Serial.begin(115200);
   dht.begin();
+  wifi_connect();
+  mqtt.setServer(MQTT_BROKER, 1883);
 }
 
 void loop()
 {
   dht.begin();
-  delay(5000);
+  if (!mqtt.connected())
+  {
+    mqtt_connect();
+  }
+  mqtt.loop();
+  publish();
+  delay(300000); // execute once every 5 minutes, don't flood remote service
+}
 
+void publish()
+{
   float h = dht.readHumidity();
   // Read temperature as Celsius (the default)
   float t = dht.readTemperature();
+  String s;
+  const int capacity = JSON_OBJECT_SIZE(3);
+  StaticJsonDocument<capacity> doc;
+  doc["temperature"] = t;
+  doc["humidity"] = h;
+  serializeJson(doc, s);
+  mqtt.publish("TiimB/feeds/sensor-test", s.c_str());
+  Serial.print("Published: ");
+  Serial.println(s);
+}
 
-  if (isnan(h) || isnan(t))
+void mqtt_connect()
+{
+  while (!mqtt.connected())
   {
-    Serial.println("Failed to read from DHT sensor!");
-    return;
+    Serial.print("Reconnecting...");
+    if (!mqtt.connect("ESP8266Client", mqtt_user, mqtt_password))
+    {
+      Serial.print("failed, rc=");
+      Serial.print(mqtt.state());
+      Serial.println(" retrying in 5 seconds");
+      delay(15000);
+    }
+  }
+  Serial.println("  Connected");
+}
+
+void wifi_connect()
+{
+  // We start by connecting to a WiFi network
+
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
   }
 
-  Serial.print("Luftfeuchtigkeit: ");
-  Serial.print(h);
-  Serial.print(" %\t");
-  Serial.print("Temperatur: ");
-  Serial.print(t);
-  Serial.println(" *C ");
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
 }
